2c620867fa098a317f35ef5a8cd20370bea4ace0
dev-salih <salihmu79@gmail.com>
2026-05-11 21:06:01 +0300

    span tag is added

--- script.js
+++ script.js
@@ -36,8 +36,7 @@ function calculateAge() {
         m3 = 11;
         y3--;
     }
-    result.innerHTML = `you are ${y3} years, ${m3} months and ${d3} days old.`;
-    
+    result.innerHTML = `you are <span>${y3}</span> years, <span>${m3}</span>months and <span>${d3}</span>days old.`;
 }
 
 function getDaysInMonth(year, month){
